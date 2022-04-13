ded474d01a5bd7dc2465a1483ec9995a7a5ff49e
santoke <czegal@hotmail.com>
2022-04-13 18:04:20 +0900

    Feat - export app

--- src/framework/web/app.ts
+++ src/framework/web/app.ts
@@ -3,13 +3,12 @@ import bodyParser from "koa-bodyparser";
 import mainRouter from "./router/MainRouter";
 import roomRouter from "./router/RoomRouter";
 
-const listen = (port: number) => {
-  const app = new Koa();
-  app.use(bodyParser());
-  app.use(mainRouter.routes());
-  app.use(roomRouter.routes());
+export const app = new Koa();
+app.use(bodyParser());
+app.use(mainRouter.routes());
+app.use(roomRouter.routes());
+
+export const listen = (port: number) => {
   app.listen(port);
   console.log(`App_is_listening_${port}`);
 };
-
-export default listen;
