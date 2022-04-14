fb52b45510d741f52a295e218088ab9070f63c14
santoke <czegal@hotmail.com>
2022-04-14 20:32:46 +0900

    Feat - init database source

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import "reflect-metadata";
-import "./ioc";
+import DataSource from "./framework/orm/DataSource";
 import { listen } from "./framework/web/app";
+import "./ioc";
 
-// TODO : temporary env vars
+// TODO : temporary env config variables
 process.env = Object.assign(process.env, {
   MAX_NUMBER_ROOM_PARTICIPATION: 100,
   MIN_NUMBER_ROOM_PARTICIPATION: 3,
   MAX_TIME_LIMIT_IN_SECONDS: 3600,
 });
 
-listen(3000);
+DataSource.initialize()
+  .then(() => {
+    listen(3000);
+  })
+  .catch((error) => {
+    console.log(`Database_initialize_error: ${error}`);
+  });
