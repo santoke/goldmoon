8dd108bd1c0e7349b03c9f6da29fdbe38b1420f9
santoke <czegal@hotmail.com>
2022-04-18 21:09:48 +0900

    Feat - PersistenceAdapter member

--- src/room/app/CreateRoomService.ts
+++ src/room/app/CreateRoomService.ts
@@ -1,7 +1,17 @@
-import { Service } from "typedi";
+import { Container, Service } from "typedi";
+import { RoomPersistenceAdapter } from "../adapter/out/RoomPersistenceAdapter";
 import { CreateRoomUseCase } from "./port/in/CreateRoomUseCase";
+import { CreateRoomDto } from "./port/out/CreateRoomDto";
 
 @Service()
 export default class CreateRoomService implements CreateRoomUseCase {
-  public createRoom() {}
+  private readonly roomPersistenceAdapter;
+
+  constructor() {
+    this.roomPersistenceAdapter = Container.get(RoomPersistenceAdapter);
+  }
+
+  public async createRoom(dto: CreateRoomDto) {
+    await this.roomPersistenceAdapter.createRoom(dto);
+  }
 }
