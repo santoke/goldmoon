fbf8c718e708c852717e2251fe65a95af85104dc
santoke <czegal@hotmail.com>
2022-04-18 21:09:21 +0900

    Fix - async function

--- src/room/adapter/out/RoomPersistenceAdapter.ts
+++ src/room/adapter/out/RoomPersistenceAdapter.ts
@@ -1,13 +1,14 @@
-import { Inject } from "typedi";
+import { Inject, Service } from "typedi";
 import { CreateRoomDto } from "../../app/port/out/CreateRoomDto";
 import { CreateRoomPort } from "../../app/port/out/CreateRoomPort";
 import { RoomEntity } from "../../domain/RoomEntity";
 import { UpsertRoomRepository } from "./UpsertRoomRepository";
 
+@Service()
 export class RoomPersistenceAdapter implements CreateRoomPort {
   constructor(@Inject("UpsertRoomRepository") private upsertRoomRepository: UpsertRoomRepository) {}
 
-  createRoom(dto: CreateRoomDto): RoomEntity {
+  async createRoom(dto: CreateRoomDto): Promise<RoomEntity> {
     return this.upsertRoomRepository.createRoom(dto);
   }
 }
