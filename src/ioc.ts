9ff92d01face751dc8c516bd3064be406df87adc
santoke <czegal@hotmail.com>
2022-04-18 21:08:12 +0900

    Fix - Container set 분리

--- src/ioc.ts
+++ src/ioc.ts
@@ -1,8 +1,6 @@
 import { Container } from "typedi";
-import CreateRoomService from "./room/app/CreateRoomService";
 import { UpsertRoomRepository } from "./room/adapter/out/UpsertRoomRepository";
+import CreateRoomService from "./room/app/CreateRoomService";
 
-Container.set([
-  { id: "CreateRoomService", value: new CreateRoomService() },
-  { id: "UpsertRoomRepository", value: new UpsertRoomRepository() },
-]);
+Container.set([{ id: "UpsertRoomRepository", value: new UpsertRoomRepository() }]);
+Container.set([{ id: "CreateRoomService", value: new CreateRoomService() }]);
